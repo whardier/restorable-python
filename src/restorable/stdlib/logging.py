3a93d77d09723f63b927ecf97d7f2c599b034ffd
Shane Spencer <305301+whardier@users.noreply.github.com>
2022-12-08 08:30:44 -0900

    update restorable logging to back up factory components (logger class, log record class, default formatter) for root manager and global scope

--- src/restorable/stdlib/logging.py
+++ src/restorable/stdlib/logging.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from logging import Logger
+from logging import Handler, Logger
 from typing import Callable, cast
 
 logger = logging.getLogger(__name__)
@@ -15,9 +15,18 @@ class RestorableLogging:
     def __enter__(self) -> None:
         _acquire_lock()
 
+        self.logger_class = logging.getLoggerClass()
+        self.log_record_factory = logging.getLogRecordFactory()
+
+        self.default_formatter = getattr(logging, "_defaultFormatter")  # noqa: B009
+
+        self.root_manager_logger_class = logging.root.manager.loggerClass
+        self.root_manager_log_record_factory = logging.root.manager.logRecordFactory
         self.root_manager_logger_dict = logging.root.manager.loggerDict
         self.root_handlers = logging.root.handlers
         self.root_filters = logging.root.filters
+
+        self.root_handler_attrs: dict[Handler, dict[str, object]] = {}
 
         self.logging_levels = {
             name: logger.level for name, logger in logging.root.manager.loggerDict.items() if isinstance(logger, Logger)
@@ -25,6 +34,14 @@ class RestorableLogging:
 
         logging.root.manager.loggerDict = self.root_manager_logger_dict.copy()
         logging.root.handlers = self.root_handlers.copy()
+
+        for handler in self.root_handlers:
+            self.root_handler_attrs[handler] = {
+                "level": getattr(handler, "level", None),
+                "formatter": getattr(handler, "formatter", None),
+                "filters": getattr(handler, "filters", None),
+            }
+
         logging.root.filters = self.root_filters.copy()
 
         _clear_cache()
@@ -33,8 +50,22 @@ class RestorableLogging:
     def __exit__(self, exc_type, exc_val, exc_tb):
         _acquire_lock()
 
+        logging.setLoggerClass(self.logger_class)
+        logging.setLogRecordFactory(self.log_record_factory)
+
+        setattr(logging, "_defaultFormatter", self.default_formatter)  # noqa: B010
+
+        logging.root.manager.loggerClass = self.root_manager_logger_class
+        logging.root.manager.logRecordFactory = self.root_manager_log_record_factory
         logging.root.manager.loggerDict = self.root_manager_logger_dict
         logging.root.handlers = self.root_handlers
+
+        for root_handler in self.root_handler_attrs:
+            _attrs = self.root_handler_attrs[root_handler]
+            setattr(root_handler, "level", _attrs["level"])  # noqa: B010
+            setattr(root_handler, "formatter", _attrs["formatter"])  # noqa: B010
+            setattr(root_handler, "filters", _attrs["filters"])  # noqa: B010
+
         logging.root.filters = self.root_filters
 
         for name, level in self.logging_levels.items():
